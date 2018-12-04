6cfd69b82f0092eee792ab3e0234dccde721cf22
Dmitryi <dmitryi.vasiliev20@gmail.com>
2018-12-05 00:32:20 +0500

    Split project on modules

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,17 @@
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
     public static void main(String [] args) throws SQLException {
-        DataBase dataBase = new DataBase();
-       // dataBase.printTableUser();
+      //  DataBase dataBase = new DataBase();
+    //User user = new User(1,"nikita");
+    //Task task = new Task("abc",1,"qwe");
+     //   List<Task> list = new ArrayList<Task>();
+    //    list.add(task);
+   // user.setTaskList(list);
+  //  System.out.println(user.getTaskList());
+        // dataBase.printTableUser();
      //  dataBase.deleteUser(0);
       //  dataBase.addUser(1,"userTest");
      //   dataBase.printTableUser();
